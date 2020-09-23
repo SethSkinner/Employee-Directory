91c250d2856df7952c185f6b7c4fa1bd303c1124
seth skinner <sethwskinner@gmail.com>
2020-09-22 20:45:40 -0500

    building out the beginning of the table index

---
+++ src/components/Table/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { useTable, UseFailures, useSortBy, useFilters } from "react-table";
+import "./style.css"
+
+export default const Table ({ columns, data }) => {
+
+    const [filterNameInput, setFilterNameInput] = useState('');
+    const [filterOccupationInput, setFilterOccupationInput] = useState('');
+    const [filterDepartmentInput, setFilterDepartmentInput] = useState('');
+
+        const {
+
+            getTableProps,
+            getTableBodyProps,
+            headerGroups,
+            rows,
+            prepareRow,
+            setilter
+
+        } = useTable (
+            {
+                columns,
+                data
+            },
+            useFilters,
+            useSortBy
+        );
+
+            const handleFilterChangeName = event => {
+                const value = event.target.value || undefined;
+                setFilter('name', value);
+                setFilterNameInput(value);
+            };
+
+            const handleFilterChangeOccupation = event => {
+                const value = event.target.value || undefined;
+                setFilter('occupation', value);
+                setFilterOccupationInput(value);
+            };
+
+            const handFilterChangeDepartment = event => {
+                const value = event.target.value || undefined;
+                setFilter('department', value);
+                setFilterDepartmentInput(value);
+            };
+
+            return (
+                
+            )
+
+} 
