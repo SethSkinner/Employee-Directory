ad7ab8564ce106ed4b5a9f94a564f2073bc6595f
seth skinner <sethwskinner@gmail.com>
2020-09-22 20:47:02 -0500

    building out table index

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -18,34 +18,45 @@ export default const Table ({ columns, data }) => {
             setilter
 
         } = useTable (
+
             {
+
                 columns,
                 data
+
             },
+
             useFilters,
             useSortBy
+
         );
 
             const handleFilterChangeName = event => {
+
                 const value = event.target.value || undefined;
                 setFilter('name', value);
                 setFilterNameInput(value);
+
             };
 
             const handleFilterChangeOccupation = event => {
+
                 const value = event.target.value || undefined;
                 setFilter('occupation', value);
                 setFilterOccupationInput(value);
+
             };
 
             const handFilterChangeDepartment = event => {
+
                 const value = event.target.value || undefined;
                 setFilter('department', value);
                 setFilterDepartmentInput(value);
+                
             };
 
             return (
-                
+
             )
 
 } 
